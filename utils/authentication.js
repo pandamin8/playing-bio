569cfa9f387c77fc3f9a4a6e6d47650826e827d2
Amin Khodabande <whfk233@gmail.com>
2022-03-25 13:47:10 +0430

    fixed spotify scopes

--- utils/authentication.js
+++ utils/authentication.js
@@ -5,25 +5,7 @@ const SpotifyWebApi = require('spotify-web-api-node')
 const router = express.Router()
 
 const scopes = [
-  'ugc-image-upload',
-  'user-read-playback-state',
-  'user-modify-playback-state',
-  'user-read-currently-playing',
-  'streaming',
-  'app-remote-control',
-  'user-read-email',
-  'user-read-private',
-  'playlist-read-collaborative',
-  'playlist-modify-public',
-  'playlist-read-private',
-  'playlist-modify-private',
-  'user-library-modify',
-  'user-library-read',
-  'user-top-read',
-  'user-read-playback-position',
-  'user-read-recently-played',
-  'user-follow-read',
-  'user-follow-modify'
+  'user-read-currently-playing'
 ]
 
 const spotifyApi = new SpotifyWebApi({
